76b93a165f71c83fb9cfd58279fd2a9fe351189e
Danila Solntsev <danilas444@gmail.com>
2022-02-20 02:39:26 +0300

    Now this code is actually working. It had wrong calculations all the time in past.

--- Numeral_System_Translator/Program.cs
+++ Numeral_System_Translator/Program.cs
@@ -4,201 +4,84 @@ using System.Diagnostics;
 
 namespace Numeral_System_Translator
 {
-    public static class Extensions
-    {
-        public static string Filter(this string str, List<char> charsToRemove)
-        {
-            foreach (char c in charsToRemove)
-            {
-                str = str.Replace(c.ToString(), String.Empty);
-            }
-
-            return str;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             Stopwatch stopwatch = new Stopwatch();
 
-            string Number = "5786734539,17943197621";
-            int toBase = 1;
-            int fromBase = 10;
-            int accuracy = 1000;
+            string answer, Number = "251,513";
+            int toBase = 16;
+            int fromBase = 8;
+            int accuracy = 6;
             stopwatch.Start();
-            string Answer = Any_To_Any(Number, toBase, fromBase, accuracy);
+            answer = AnyToAny(Number, toBase, fromBase, accuracy);
             stopwatch.Stop();
-            Console.WriteLine(Answer);
+            Console.WriteLine(answer);
             Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
         }
 
-        /// <summary>Converts numbers from any numeral system between 2 and 16. </summary>
-        /// <returns> Will return Number itself if toBase is equal fromBase. Will return "Error" if  toBase or fromBase is less than 2 or bigger than 16</returns>
-        private static string Any_To_Any(string Number, int toBase, int fromBase, int accuracy)
+        public static string ConvertToDecimal(string number, int fromBase)
         {
-
-            //// Check for mistakes in input ////
+            string[] num = number.Split(',');
+            double numberBeforeFloat = 0;
+            for (int i = 0; i<num[0].Length; i++)
+                numberBeforeFloat = numberBeforeFloat + Convert.ToInt32(num[0][i].ToString(), fromBase) * Math.Pow(fromBase, num[0].Length-i-1);
+            if (num.Length == 1)
+                return numberBeforeFloat.ToString();
+            else
+            {
+                double numberAfterFloat = 0;
+                for (int i = 0; i < num[1].Length; i++)
+                    numberAfterFloat = numberAfterFloat + Convert.ToInt32(num[1][i].ToString(), fromBase) * Math.Pow(fromBase, -1*i-1);
+                return (numberBeforeFloat + numberAfterFloat).ToString();
+            }
+        }
+        public static string ConvertFromDecimal(string number, int toBase, int accuracy)
+        {
+            string[] num = number.Split(',');
+            long temp_i = Convert.ToInt64(num[0]);
+            string numBeforeFloat = null;
+            while (temp_i > 0)
+            {
+                numBeforeFloat = (temp_i % toBase).ToString("X") + numBeforeFloat;
+                temp_i = temp_i / toBase;
+            }
+            if (num.Length == 1)
+                return numBeforeFloat;
+            else
+            {
+                string numAfterFloat = "";
+                double floatPart = Convert.ToDouble($"0,{num[1]}");
+                while (floatPart != 0)
+                {
+                    floatPart = floatPart * toBase;
+                    numAfterFloat += Convert.ToInt64(Math.Truncate(floatPart)).ToString("X");
+                    floatPart = floatPart - Math.Truncate(floatPart);
+                    if (numAfterFloat.Length == accuracy)
+                        break;
+                }
+                return numBeforeFloat + "," + numAfterFloat;
+            }
+        }
+        public static string AnyToAny(string Number, int toBase, int fromBase, int accuracy)
+        {
             if (toBase == fromBase)
                 return Number;
-
             if (toBase < 2 | fromBase < 2 | toBase > 16 | fromBase > 16)
-                return "Error";
-
-            foreach (char ch in Number)
-            {
-                string tmp_s1 = ch.ToString();
-                if (tmp_s1 != ",")
-                {
-                    int tmp_i = int.Parse(tmp_s1, System.Globalization.NumberStyles.HexNumber);
-                    if (tmp_i > fromBase)
-                        return ($"This is not {fromBase} based number");
-                }
-            }
-
-            //// Code itself ////
-            string value1;
-            string value2 = null;
-
+                throw new Exception("Please use base 2-16");
             var number = Number.Split(",");
-
-            List<char> charsToRemove = new List<char>() { ',', '.' };
-            string tmp_s = null;
-
-            char[] numBeforeFloat = number[0].ToCharArray();
-
-            for (int i = 0; i < numBeforeFloat.Length / 2; i++)
-            {
-                char tmp = numBeforeFloat[i];
-                numBeforeFloat[i] = numBeforeFloat[numBeforeFloat.Length - i - 1];
-                numBeforeFloat[numBeforeFloat.Length - i - 1] = tmp;
-            }
-
-            // Перевод в десятичную систему числа перед запятой
-            if (fromBase != 10)
-            {
-                int index = 0;
-                double numberBeforeFloat = 0;
-                foreach (char ch in numBeforeFloat)
-                {
-                    string num = ch.ToString();
-                    long tmp_i = long.Parse(num, System.Globalization.NumberStyles.HexNumber);
-
-                    numberBeforeFloat = numberBeforeFloat + tmp_i * Math.Pow(fromBase, index);
-
-                    index++;
-                }
-                tmp_s = numberBeforeFloat.ToString();
-            }
+            char[] tmpChar;
+            if (number.Length == 1)
+                tmpChar = number[0].ToCharArray();
             else
-            {
-                foreach (char ch in numBeforeFloat)
-                {
-                    tmp_s = tmp_s + ch;
-                }
-            }
+                tmpChar = (number[0]+number[1]).ToCharArray();
 
-            // Перевод из десятичной системы в другие
-            if (toBase != 10)
-            {
-                long temp_i = Convert.ToInt64(tmp_s);
-                tmp_s = null;
-                while (temp_i != 0)
-                {
-                    tmp_s = Convert.ToString(temp_i % toBase) + tmp_s;
-                    temp_i = temp_i / toBase;
-                }
-                value1 = tmp_s;
-            }
-            else
-            {
-                value1 = tmp_s;
-            }
-            // Проверка на наличие запятой
-            if (number.Length != 2)
-            {
-                return value1;
-            }
-            else
-            {
-                char[] numAfterFloat = number[1].ToCharArray();
-                for (int i = 0; i < numAfterFloat.Length / 2; i++)
-                {
-                    char tmp = numAfterFloat[i];
-                    numAfterFloat[i] = numAfterFloat[numAfterFloat.Length - i - 1];
-                    numAfterFloat[numAfterFloat.Length - i - 1] = tmp;
-                }
-                tmp_s = null;
-                // Перевод в десятичную систему числа после запятой
-                if (fromBase != 10)
-                {
-                    foreach (char ch in numAfterFloat)
-                    {
-                        string num = ch.ToString();
-                        double tmp_d = int.Parse(num, System.Globalization.NumberStyles.HexNumber);
+            foreach (char ch in tmpChar)
+                if (int.Parse(ch.ToString(), System.Globalization.NumberStyles.HexNumber) > fromBase-1)
+                    throw new Exception($"This number is not {fromBase} based");
 
-                        tmp_s = Convert.ToString(tmp_d) + tmp_s;
-                        tmp_s = Convert.ToString(Convert.ToDouble(tmp_s) / fromBase);
-                        tmp_s = tmp_s.Filter(charsToRemove);
-                        if (tmp_s.StartsWith("0"))
-                        {
-                            tmp_s = tmp_s.Remove(0, 1);
-                        }
-                    }
-                    value2 = tmp_s;
-                }
-                else
-                {
-                    foreach (char ch in numAfterFloat)
-                    {
-                        tmp_s = tmp_s + ch;
-                    }
-                }
-
-                // Перевод в нужную систему числа после запятой НЕ РАБТАЕТ С ДВОИЧНЫМ И ДЕСЯТЕРИЧНЫМ
-                if (toBase != 10)
-                {
-                    long j_i = Convert.ToInt64(tmp_s);
-                    value2 = null;
-                    int arr_count = tmp_s.Length;
-                    tmp_s = null;
-                    while (j_i != 0)
-                    {
-                        if (tmp_s != null)
-                        {
-                            j_i = Convert.ToInt64(tmp_s);
-                        }
-
-                        j_i = j_i * toBase;
-                        tmp_s = j_i.ToString();
-
-                        if (value2 != null)
-                        {
-                            if (value2.Length == accuracy + 1)
-                            {
-                                break;
-                            }
-                        }
-
-                        if (tmp_s.Length == arr_count + 2)
-                        {
-                            string tmp_s1 = Convert.ToInt32(tmp_s.Substring(0, 2)).ToString("X");
-                            value2 = value2 + tmp_s1;
-                            tmp_s = tmp_s.Remove(0, 2);
-                        }
-                        else if (tmp_s.Length == arr_count + 1)
-                        {
-                            value2 = value2 + tmp_s.Substring(0, 1);
-                            tmp_s = tmp_s.Remove(0, 1);
-                        }
-                        else
-                            value2 = value2 + "0";
-                    }
-                    value2 = value2.Remove(value2.Length - 1, 1);
-                }
-                return value1 + "," + value2;
-            }
+            return ConvertFromDecimal(ConvertToDecimal(Number, fromBase), toBase, accuracy);
         }
     }
 }
